d0de1f03b683c3e06b8ed1b596534700de81463e
GokaPek <Hgogogo@yandex.ru>
2024-12-27 13:38:18 +0400

    init

---
+++ src/main/java/ru/promo/consul_plan_notify/ConsulPlanNotifyApplication.java
@@ -0,0 +1,13 @@
+package ru.promo.consul_plan_notify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulPlanNotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulPlanNotifyApplication.class, args);
+	}
+
+}
